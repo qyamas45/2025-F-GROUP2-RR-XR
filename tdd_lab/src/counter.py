e43c340391e29c2d07ccdea178539027bd23827f
qyamas45 <qyamas@hotmail.com>
2025-09-10 18:29:10 -0700

    removed tdd_lab submodule

---
+++ tdd_lab/src/counter.py
@@ -0,0 +1,33 @@
+"""
+Counter API Implementation
+"""
+from flask import Flask, jsonify
+from . import status
+
+app = Flask(__name__)
+
+COUNTERS = {}
+
+
+def counter_exists(name):
+  """Check if counter exists"""
+  return name in COUNTERS
+
+@app.route('/counters/<name>', methods=["GET", "POST"])
+def create_counter(name):
+    """Create a counter"""
+    # ===========================
+    # Test: check_duplicated_counter
+    # Author: Alex Yamasaki
+    # Date: 2025-09-10
+    # Description: PREVENT DUPLCIATED counters
+    # ===========================
+    """Check duplicated counter"""
+    if counter_exists(name):
+        return jsonify({"error": f"Counter {name} already exists"}), status.HTTP_409_CONFLICT
+    
+    COUNTERS[name] = 0
+    return jsonify({name: COUNTERS[name]}), status.HTTP_201_CREATED
+
+ 
+   
---
+++ tdd_lab/tests/test_counter.py
@@ -0,0 +1,44 @@
+"""
+Test Cases for Counter Web Service
+
+Create a service that can keep a track of multiple counters
+- API must be RESTful - see the status.py file. Following these guidelines, you can make assumptions about
+how to call the web service and assert what it should return.
+- The endpoint should be called /counters
+- When creating a counter, you must specify the name in the path.
+- Duplicate names must return a conflict error code.
+- The service must be able to update a counter by name.
+- The service must be able to read the counter
+"""
+import pytest
+from src import app
+from src import status
+
+@pytest.fixture()
+def client():
+    """Fixture for Flask test client"""
+    return app.test_client()
+
+@pytest.mark.usefixtures("client")
+class TestCounterEndpoints:
+    """Test cases for Counter API"""
+
+    def test_create_counter(self, client):
+        """It should create a counter"""
+        result = client.post('/counters/foo')
+        assert result.status_code == status.HTTP_201_CREATED
+    
+    # ===========================
+    # Test: check_duplicated_counter
+    # Author: Alex Yamasaki
+    # Date: 2025-09-10
+    # Description: PREVENT DUPLCIATED counters
+    # ===========================
+    def check_duplicated_counter(self, client):
+        #Assume creation is worked
+        client.post('/counters/foo')
+        result = client.post('/counters/foo')
+        assert result.status_code == status.HTTP_409_CONFLICT
+        assert b"Already exists" in result.data
+        
+    
