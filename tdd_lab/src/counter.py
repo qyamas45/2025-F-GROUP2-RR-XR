d08a248c0858423a5967e92383b5a062a0b7ddf0
CrawlLaws <janda02@protonmail.com>
2025-09-12 11:59:17 -0700

    Added delete functionality for counter

---
+++ tdd_lab/src/counter.py
@@ -0,0 +1,31 @@
+"""
+Counter API Implementation
+"""
+from flask import Flask, jsonify
+from . import status
+
+app = Flask(__name__)
+
+COUNTERS = {}
+
+def counter_exists(name):
+  """Check if counter exists"""
+  return name in COUNTERS
+
+@app.route('/counters/<name>', methods=["GET","POST"])
+def create_counter(name):
+    """Create a counter"""
+    if counter_exists(name):
+        return jsonify({"error": f"Counter {name} already exists"}), status.HTTP_409_CONFLICT
+    COUNTERS[name] = 0
+    return jsonify({name: COUNTERS[name]}), status.HTTP_201_CREATED
+
+
+@app.route('/counters/<name>', methods=['DELETE'])
+def delete_counter(name):
+    """Delete a counter"""
+    if not counter_exists(name):
+        return jsonify({"error": f"Counter {name} does not exists"}), status.HTTP_404_NOT_FOUND
+    
+    del COUNTERS[name]
+    return '', status.HTTP_204_NO_CONTENT
